89cbe300feb8fa205cbab6176a837dcd03c7ac3b
Matt McCormick <matt.mccormick@kitware.com>
2022-10-06 17:11:20 -0400

    feat: Add CategoricalColorNames.ts

---
+++ src/CategoricalColorNames.ts
@@ -0,0 +1,13 @@
+const CategoricalColorNames: string[] = [
+  'glasbey',
+  'glasbey_light',
+  'glasbey_warm',
+  'modulate',
+
+  'glasbey_bw',
+  'glasbey_dark',
+  'glasbey_cool',
+  'modulate_dark',
+]
+
+export default CategoricalColorNames
