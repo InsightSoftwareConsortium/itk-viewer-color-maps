545e1d9d6ffbe4601f206d6f5e8d339830ef1a4e
Matt McCormick <matt.mccormick@kitware.com>
2022-10-06 22:51:35 -0400

    refactor: Change CategoricalColors model, also output JSON

--- src/generateCategoricalColors.py
+++ src/generateCategoricalColors.py
@@ -2,6 +2,7 @@
 
 import csv
 from pathlib import Path
+import json
 
 glasbey_dir = Path('/home/matt/src/colorcet/assets/Glasbey')
 
@@ -14,7 +15,7 @@ glasbey_to_name = {
         'glasbey_bw_minc_20_maxl_70': 'glasbey_dark',
         }
 
-colors = []
+colors = {}
 for file_prefix, name in glasbey_to_name.items():
     csv_filepath = glasbey_dir / Path(file_prefix + '_n256.csv')
     print(csv_filepath, csv_filepath.exists(), name)
@@ -22,36 +23,40 @@ for file_prefix, name in glasbey_to_name.items():
     with open(csv_filepath) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            rgb_values.append(row)
+            rgb_values.extend([float(f) for f in row])
     indexed_color = {
             'IndexedColors': rgb_values,
-            'Name': name
+            'NanColor': [0.75, 0.75, 0.75],
             }
-    colors.append(indexed_color)
+    colors[name] = indexed_color
+
+rgb_values = []
+for ii in range(12):
+    rgb_values.extend([1.0,1.0,1.0])
+indexed_color = {
+        'IndexedColors': rgb_values,
+        'NanColor': [1.0, 0.0, 0.0],
+        }
+colors['modulate'] = indexed_color
+
+rgb_values = []
+for ii in range(12):
+    rgb_values.extend([0.0,0.0,0.0])
+indexed_color = {
+        'IndexedColors': rgb_values,
+        'NanColor': [1.0, 0.0, 0.0],
+        }
+colors['modulate_dark'] = indexed_color
+
+with open('src/CategoricalColors.json', 'w') as fp:
+    json.dump(colors, fp, indent=2)
 
 with open('src/CategoricalColors.ts', 'w') as fp:
-    fp.write('const CategoricalColors = new Map<string, Array<[number, number, number]>>()\n\n')
-    for color in colors:
-        fp.write("CategoricalColors.set('{}', [\n".format(color['Name']))
-        rgb_values = color['IndexedColors']
-        for rgb in rgb_values:
-            fp.write('  [')
-            fp.write(str(float(rgb[0])))
-            fp.write(',')
-            fp.write(str(float(rgb[1])))
-            fp.write(',')
-            fp.write(str(float(rgb[2])))
-            fp.write('],\n')
-        fp.write('])\n')
 
-    fp.write("\nCategoricalColors.set('modulate', [\n")
-    for ii in range(12):
-        fp.write('  [1.0,1.0,1.0],\n')
-    fp.write('])\n')
-
-    fp.write("\nCategoricalColors.set('modulate_dark', [\n")
-    for ii in range(12):
-        fp.write('  [0.0,0.0,0.0],\n')
-    fp.write('])\n')
+    fp.write('const CategoricalColors = new Map<string, Record<"IndexedColors" | "NanColor", number[]>>()\n\n')
+    for color_name, value in colors.items():
+        fp.write("CategoricalColors.set('{}',\n".format(color_name))
+        fp.write(json.dumps(value, indent=2))
+        fp.write(')\n\n')
 
     fp.write('\nexport default CategoricalColors\n')
